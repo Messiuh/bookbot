88cd1ae1eaf1c200678c17347373dab5c08a3e17
messiuh <bleonard1993@icloud.com>
2025-04-28 17:13:08 -0700

    added moby_dick, pride_and_prejudice

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+import sys
 from stats import count_words, count_characters, sort_dict
-
 def get_book_text(path):
     with open(path) as f:
         file_contents = f.read()
     return file_contents 
 
 def main():
-    book_path ="books/frankenstein.txt"
-    book_text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
+    book_text = get_book_text(book_path)
     word_count = count_words(book_text)
     char_dict = count_characters(book_text)
     report = sort_dict(char_dict)
